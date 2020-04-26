044051bbc5f62bc84a7c57e7c6aa5459a440c2f6
Chris <chriseckert.ce@gmail.com>
2020-04-26 09:32:18 +0200

    Add helper comment

--- utils/createFile.js
+++ utils/createFile.js
@@ -3,6 +3,7 @@ const defineFile = require("./defineFile");
 
 const createFile = (path, name, extension) => {
   fs.writeFile(
+    // Remove ternary operator if all files should have the same naming 
     `${path}${extension === ".ts" ? "index" : name}${extension}`,
     defineFile(name, extension),
     (error) => {
