a324e7d47a7737a00a25661f8cab37516518b4ab
Chris <chriseckert.ce@gmail.com>
2020-04-26 08:19:50 +0200

    Setup validation and create structure

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-const fs = require('fs');
+const fs = require('fs')
 const defineEntryFile = require("./templates/entry")
 const defineMainFile = require("./templates/main")
 
 const path = process.argv[2]
 const name = process.argv[3]
 
-console.log(path, name)
+const validatePath = (path) => {
+  const lastCharacter = path[path.length - 1]
 
-fs.writeFile(`${name}.ts`, defineEntryFile(name), function (err) {
-  if (err) throw err;
-  console.log('Added component entry file');
-});
+  return lastCharacter === "/" || lastCharacter === "\\"
+}
 
-fs.writeFile(`${name}.tsx`, defineMainFile(name), function (err) {
-  if (err) throw err;
-  console.log('Added component main file');
-});
+if (path && name) {
 
-fs.writeFile(`${name}.scss`, ``, function (err) {
-  if (err) throw err;
-  console.log('Add styles file');
-});
+  if (validatePath(path)) {
+    fs.mkdir(`${path}${name}`, (error) => {
+      if (error) return console.log(`Error: ${error}`)
+      console.log("folder was created")
+
+      const newPath = `${path}${name}/`
+
+      fs.writeFile(`${newPath}${name}.ts`, defineEntryFile(name), function (err) {
+        if (err) throw err
+        console.log('Added component entry file')
+      })
+
+      fs.writeFile(`${newPath}${name}.tsx`, defineMainFile(name), function (err) {
+        if (err) throw err
+        console.log('Added component main file')
+      })
+
+      fs.writeFile(`${newPath}${name}.scss`, ``, function (err) {
+        if (err) throw err
+        console.log('Add styles file')
+      })
+
+    })
+
+  }
+
+  console.log("Please supply valid path")
+} else {
+  console.log("Please supply both a path and name")
+}
